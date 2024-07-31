af845f659d6b2935c235df630e265945b749248a
曹鑫 <caoxin@baicizhan.com>
2024-07-31 11:56:33 +0800

    配置项注释

--- extractor.config.js
+++ extractor.config.js
@@ -1,4 +1,6 @@
+// https://github.com/isaacs/node-glob#readme 
+// include前面会自动加上当前项目的路径,所以/指向的是当前项目
 module.exports = {
-  include: ['/**/*.{js,jsx}', '/**/*.{ts,tsx}'], // 只能写文件
+  include: ['/**/*.js{,x}', '/**/*.ts{,x}'], // 只能写文件
   exclude: ['**/*[Dd]emo*']
 }
