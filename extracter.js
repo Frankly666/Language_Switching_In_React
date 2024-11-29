8994167366b9423f67261659ff7ec8d7e46247fb
Frankly <xushuai@baicizhan.com>
2024-11-29 16:04:27 +0800

    fix: :bug: 文件排除有问题

--- extracter.js
+++ extracter.js
@@ -1,37 +1,49 @@
-const fs = require('fs')
-const path = require('path')
-const glob = require('glob')
-const hanRegex = /[，。？《》「」【】（）、·～！\u3400-\u4DB5\u4E00-\u9FEA\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29\u{20000}-\u{2A6D6}\u{2A700}-\u{2B734}\u{2B740}-\u{2B81D}\u{2B820}-\u{2CEA1}\u{2CEB0}-\u{2EBE0}]+/u
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const hanRegex =
+  /[，。？《》「」【】（）、·～！\u3400-\u4DB5\u4E00-\u9FEA\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29\u{20000}-\u{2A6D6}\u{2A700}-\u{2B734}\u{2B740}-\u{2B81D}\u{2B820}-\u{2CEA1}\u{2CEB0}-\u{2EBE0}]+/u;
 const commentRegex = /\/\/.*|\/\*[\s\S]*?\*\/|^\s*\*.*/;
-const config = require('./extractor.config')
+const config = require("./extractor.config");
 
-if (fs.existsSync('dist')) fs.rmSync('dist', { recursive: true })
-  const distPath = path.resolve('dist')
-fs.mkdirSync(distPath)
-const distRowPath = path.resolve(distPath, 'row')
-fs.mkdirSync(distRowPath)
-const distMapPath = path.resolve(distPath, 'map')
-fs.mkdirSync(distMapPath)
+if (fs.existsSync("dist")) fs.rmSync("dist", { recursive: true });
+const distPath = path.resolve("dist");
+fs.mkdirSync(distPath);
+const distRowPath = path.resolve(distPath, "row");
+fs.mkdirSync(distRowPath);
+const distMapPath = path.resolve(distPath, "map");
+fs.mkdirSync(distMapPath);
 
-const entryPath = 'src/'
+const entryPath = "src/";
 
 function checked(curProject, map, row) {
-  const filePaths = glob.sync(config.include.map(value => curProject+ value), {ignore: config.exclude})
-  for(const filePath of filePaths) {
-    const content = fs.readFileSync(filePath, 'utf-8').toString()
-    const lines = content.split('\n')
+  const filePaths = glob.sync(
+    config.include.map((value) => curProject + value),
+    { ignore: config.exclude }
+  );
+
+  console.log(filePaths)
+
+  for (const filePath of filePaths) {
+    const content = fs.readFileSync(filePath, "utf-8").toString();
+    const lines = content.split("\n");
     for (let i = 0; i < lines.length; i++) {
       const line = lines[i];
-      const str = line.replace(commentRegex, '');
+      const str = line.replace(commentRegex, "");
       if (hanRegex.test(str)) {
         let _line = line;
         while (_line.match(hanRegex)) {
-          let ChWordInfo = _line.match(hanRegex)
-          const id = `${filePath.replace(/[\/\\\.]/g, '_')}-${i}-${ChWordInfo.index}`.slice(4)
+          let ChWordInfo = _line.match(hanRegex);
+          const id = `${filePath.replace(/[\/\\\.]/g, "_")}-${i}-${
+            ChWordInfo.index
+          }`.slice(4);
           row.push({
-            [id]: ChWordInfo[0]
-          })
-          _line = _line.replace(hanRegex, new Array(ChWordInfo[0].length).fill('-').join(''))
+            [id]: ChWordInfo[0],
+          });
+          _line = _line.replace(
+            hanRegex,
+            new Array(ChWordInfo[0].length).fill("-").join("")
+          );
           map.push({
             path: filePath,
             id,
@@ -39,26 +51,36 @@ function checked(curProject, map, row) {
             chn: ChWordInfo[0],
             lineNum: i, // 索引
             startAt: ChWordInfo.index,
-            length: ChWordInfo[0].length
-          })
+            length: ChWordInfo[0].length,
+          });
         }
       }
     }
   }
 }
 
-const projects = fs.readdirSync(entryPath)
+const projects = fs.readdirSync(entryPath);
+
 for (const project of projects) {
-  const map = []
-  const row = []
-  const curProject = entryPath + project
-  checked(curProject, map, row)
-  fs.writeFileSync(path.join(distMapPath, `${project}.map.json`), JSON.stringify(map), {
-    encoding: 'utf-8',
-    flag: 'w'
-  })
-  fs.writeFileSync(path.join(distRowPath, `${project}.dic.json`), JSON.stringify(row), {
-    encoding: 'utf-8',
-    flag: 'w'
-  })
+  if (project === "tools") return;
+  const map = [];
+  const row = [];
+  const curProject = entryPath + project;
+  checked(curProject, map, row);
+  fs.writeFileSync(
+    path.join(distMapPath, `${project}.map.json`),
+    JSON.stringify(map),
+    {
+      encoding: "utf-8",
+      flag: "w",
+    }
+  );
+  fs.writeFileSync(
+    path.join(distRowPath, `${project}.dic.json`),
+    JSON.stringify(row),
+    {
+      encoding: "utf-8",
+      flag: "w",
+    }
+  );
 }
--- extractor.config.js
+++ extractor.config.js
@@ -2,5 +2,10 @@
 // include前面会自动加上当前项目的路径,所以/指向的是当前项目
 module.exports = {
   include: ['/**/*.js{,x}', '/**/*.ts{,x}'], // 只能写文件
-  exclude: ['**/*[Dd]emo*']
+  exclude: [
+    '**/node_modules/**',     // 排除 node_modules
+    '**/dist/**',             // 排除 dist
+    '**/*.d.ts',              // 排除类型声明文件
+    '**/build/**',            // 排除构建目录
+  ]
 }
